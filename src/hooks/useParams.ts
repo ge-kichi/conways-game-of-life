5b8dfd786e66ccb6db8b7aa97fa8bfc0d300b366
Rikuo Kono <terrestrial1116@gmail.com>
2022-04-05 20:56:00 +0900

    add Patterns

--- src/hooks/useParams.ts
+++ src/hooks/useParams.ts
@@ -1,6 +1,7 @@
 import { reactive, computed } from "vue";
 import { useStore } from "vuex";
 import { key, GetterTypes, MutationTypes } from "@/store";
+import { Patterns } from "@/modules/CGOL";
 
 const { Gen, Pattern } = GetterTypes;
 const { UpdatePattern } = MutationTypes;
@@ -9,7 +10,7 @@ const useParams = () => {
   const { commit, getters } = useStore(key);
 
   const genContent = computed(() => getters[Gen]);
-  const patternOptions = reactive(["random", "grider"]);
+  const patternOptions = reactive(Patterns);
   const patternSelected = computed({
     get: () => getters[Pattern],
     set: (value: string) => commit(UpdatePattern, value),
