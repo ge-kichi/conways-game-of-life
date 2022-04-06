15eb2f98c48e56b98f82675d5b963bbe18abb2a8
Rikuo Kono <terrestrial1116@gmail.com>
2022-04-07 00:36:22 +0900

    リファクタリング #1

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -24,63 +24,63 @@ const useCanvas = () => {
   const { commit, getters } = useStore(key);
   const sketchIn = ref();
 
+  const clear = () => {
+    clearInterval(intervalID);
+    context.clearRect(0, 0, canvasWidth, canvasHeight);
+    commit(UpdateGen, 0);
+  };
+
+  const visualizer = (state: Int8Array[]) => {
+    for (let i = 0; i < state.length; i++) {
+      const column = state[i];
+      const y = i * cellRatio;
+      for (let j = 0; j < column.length; j++) {
+        const x = j * cellRatio;
+        if (column[j] === 1) {
+          context.fillRect(x, y, cellSize, cellSize);
+        } else {
+          context.clearRect(x, y, cellSize, cellSize);
+        }
+      }
+    }
+  };
+
+  const play = () => {
+    clear();
+    cgol = create(spaceSize, maxGen, getters[Pattern]);
+    context.fillStyle = cellStyle;
+    visualizer(cgol.state);
+    intervalID = setInterval(() => {
+      cgol = cgol.generate();
+      visualizer(cgol.state);
+      commit(UpdateGen, cgol.gen);
+    }, 1000 / 30);
+  };
+
+  const init = (node: HTMLCanvasElement) => {
+    node.removeEventListener("click", play);
+    clear();
+    const clientWidth = node.clientWidth;
+    const clientHeight = node.clientHeight;
+    spaceSize = Math.floor(clientWidth / cellRatio);
+    maxGen = Math.floor(clientHeight / cellRatio) - 1;
+    canvasWidth = spaceSize * cellRatio;
+    canvasHeight = maxGen * cellRatio;
+    node.setAttribute("width", canvasWidth.toString());
+    node.setAttribute("height", canvasHeight.toString());
+    node.addEventListener("click", play);
+  };
+
   onMounted(() => {
     const node = sketchIn.value;
     const playState = computed<PlayState>(() => getters[PlayState]);
     context = node.getContext("2d");
 
-    const clear = () => {
-      clearInterval(intervalID);
-      context.clearRect(0, 0, canvasWidth, canvasHeight);
-      commit(UpdateGen, 0);
-    };
-
-    const init = () => {
-      node.removeEventListener("click", play);
-      clear();
-      const clientWidth = node.clientWidth;
-      const clientHeight = node.clientHeight;
-      spaceSize = Math.floor(clientWidth / cellRatio);
-      maxGen = Math.floor(clientHeight / cellRatio) - 1;
-      canvasWidth = spaceSize * cellRatio;
-      canvasHeight = maxGen * cellRatio;
-      node.setAttribute("width", canvasWidth.toString());
-      node.setAttribute("height", canvasHeight.toString());
-      node.addEventListener("click", play);
-    };
-
-    const visualizer = (state: Int8Array[]) => {
-      for (let i = 0; i < state.length; i++) {
-        const column = state[i];
-        const y = i * cellRatio;
-        for (let j = 0; j < column.length; j++) {
-          const x = j * cellRatio;
-          if (column[j] === 1) {
-            context.fillRect(x, y, cellSize, cellSize);
-          } else {
-            context.clearRect(x, y, cellSize, cellSize);
-          }
-        }
-      }
-    };
-
-    const play = () => {
-      clear();
-      cgol = create(spaceSize, maxGen, getters[Pattern]);
-      context.fillStyle = cellStyle;
-      visualizer(cgol.state);
-      intervalID = setInterval(() => {
-        cgol = cgol.generate();
-        visualizer(cgol.state);
-        commit(UpdateGen, cgol.gen);
-      }, 1000 / 30);
-    };
-
     window.addEventListener("resize", () => {
       clearTimeout(timeoutID);
-      timeoutID = setTimeout(init, 1000);
+      timeoutID = setTimeout(() => init(node), 1000);
     });
-    init();
+    init(node);
 
     watchEffect(() => {
       switch (playState.value) {
